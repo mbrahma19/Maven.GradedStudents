f21b258ded2915d3458425194349c72bd0bbf67f
Marshilla Brahma <marshillabrahma@zipcoders-MacBook-Pro-10.local>
2019-03-06 17:18:37 -0500

    Completed Student and student test

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,157 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Student {
+    /**
+     * Fields in the Student class
+     *
+     */
+    private String firstName;
+    private String lastName;
+    private List<Double> examScores;
+
+    /**
+     * Nullary constructor which sets the default values
+     */
+    public Student(){
+        this("Marshilla","Brahma", new Double[0]);
+    }
+
+    /**
+     * Constructor which takes in 3 parameters
+     * @param firstName
+     * @param lastName
+     * @param examScores
+     */
+
+    public Student(String firstName, String lastName, Double[] examScores){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.examScores = new ArrayList(Arrays.asList(examScores));
+    }
+
+    /**
+     * Method to retrieve String first name
+     * @return firstName
+     */
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     *  Method to set String first name
+     * @param firstName
+     */
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     *  Method to retrieve String last name
+     * @return lastName
+     */
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * Method to set String last name
+     * @param lastName
+     */
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * Method returns the number of exams taken by the Student
+     * @return
+     */
+
+    public Integer getNumberOfExamsTaken(){
+        return examScores.size();
+    }
+
+    /**
+     * Adds an exam score to the existing exam score list
+     * @param examScore
+     */
+
+    public void addExamScore(Double examScore){examScores.add(examScore);}
+
+    /**
+     * Retrieves the exam score at the specified index
+     * @param index
+     * @return
+     */
+
+    public Double getExamScore(Integer index){return examScores.get(index);}
+
+    /**
+     *  Sets the exam score at the specified index with the new exam score provided
+     * @param examIndex
+     * @param newExamScore
+     */
+
+    public void setExamScore(int examIndex, double newExamScore){
+        examScores.remove(examIndex);
+        examScores.add(examIndex,newExamScore);
+    }
+
+    /**
+     * Returns the average score which the student has from the existing list of exam scores
+     * @return
+     */
+    public Double getAverageExamScore(){
+        return (examScores.size() == 0) ? 0.0 : Math.round(getTotalScore()/examScores.size());
+    }
+
+    /**
+     * Returns the total amount of points the student has accumalated. Calculated by adding all the exam scores.
+     * @return
+     */
+    private Double getTotalScore(){
+        Double result = 0.0;
+        for(Double d : examScores){
+            result += d;
+        }
+        return result;
+    }
+
+    /**
+     * Returns the string format of the Student object
+     * @return
+     */
+    @Override
+    public String toString() {
+        return String.format("Student Name: %s %s\n>Average Score: %.1f\n>%s",firstName, lastName,getAverageExamScore(),getExamScores());
+    }
+
+    /**
+     * Returns the string format of the exam scores
+     * @return
+     */
+    //TODO : reduce the number lines of code
+    public String getExamScores() {
+        String result = "Exam Scores:\n";
+        int i = 1;
+        for(Double d : examScores){
+            if(d == null){
+                result += String.format("       Exam %d -> null\n", i);
+                i++;
+            }else {
+                result += String.format("       Exam %d -> %.2f\n", i, d);
+                i++;
+            }
+        }
+        return result;
+    }
+
+
 }
--- src/test/java/io/zipcoder/StudentTest.java
+++ src/test/java/io/zipcoder/StudentTest.java
@@ -1,5 +1,249 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class StudentTest {
+    @Test
+    public void nullaryConstructorTest(){
+        //given
+        String firstName = "Marshilla";
+        String lastName = "Brahma";
+        Integer numberOfExams = 0;
+        //when
+        Student newStudent = new Student();
+        String actualFirst = newStudent.getFirstName();
+        String actualLast = newStudent.getLastName();
+        Integer actualNumberoFExams = newStudent.getNumberOfExamsTaken();
+        //then
+        Assert.assertEquals(firstName,actualFirst);
+        Assert.assertEquals(lastName,actualLast);
+        Assert.assertEquals(numberOfExams,actualNumberoFExams);
+    }
+
+    @Test
+    public void constructorTest(){
+        //given
+        String firstName = "Marci";
+        String lastName = "B";
+        Integer numberOfExams = 3;
+        //when
+        Student newStudent = new Student(firstName,lastName,new Double[]{97.8, 92.0, 76.9});
+        String actualFirst = newStudent.getFirstName();
+        String actualLast = newStudent.getLastName();
+        Integer actualNumberOfExams = newStudent.getNumberOfExamsTaken();
+        //then
+        Assert.assertEquals(firstName,actualFirst);
+        Assert.assertEquals(lastName,actualLast);
+        Assert.assertEquals(numberOfExams,actualNumberOfExams);
+    }
+
+    @Test
+    public void getExamScoresTestWithNoScores(){
+        //given
+        String firstName = "Marci";
+        String lastName = "B";
+        String expectedExamScores = "Exam Scores:\n";
+        //when
+        Student newStudent = new Student(firstName,lastName,new Double[0]);
+        String actualExamScoresString = newStudent.getExamScores();
+
+        //then
+        Assert.assertEquals(expectedExamScores,actualExamScoresString);
+    }
+
+    @Test
+    public void getExamScoresTestWithNull(){
+        //given
+        String firstName = "Marci";
+        String lastName = "B";
+        String expectedExamScores = "Exam Scores:\n" +
+                "       Exam 1 -> null\n" +
+                "       Exam 2 -> 92.00\n" +
+                "       Exam 3 -> null\n";
+        //when
+        Student newStudent = new Student(firstName,lastName,new Double[]{null, 92.0,null});
+        String actualExamScoresString = newStudent.getExamScores();
+
+        //then
+        Assert.assertEquals(expectedExamScores,actualExamScoresString);
+    }
+
+    @Test
+    public void setGetFirstNameTest(){
+        //given
+        String firstName = "Marci";
+        Student newStudent = new Student();
+        //when
+        newStudent.setFirstName(firstName);
+        String actualFirst = newStudent.getFirstName();
+
+        //then
+        Assert.assertEquals(firstName,actualFirst);
+    }
+
+    @Test
+    public void setGetLastNameTest(){
+        //given
+        String lastName = "B-hama";
+        Student newStudent = new Student();
+        //when
+        newStudent.setLastName(lastName);
+        String actualFirst = newStudent.getLastName();
+
+        //then
+        Assert.assertEquals(lastName,actualFirst);
+    }
+
+    @Test
+    public void addExamScoreToEmptyListCheckStringTest(){
+        //given
+        String firstName = "Marci";
+        String lastName = "B";
+        String expectedExamScores = "Exam Scores:\n" +
+                "       Exam 1 -> 97.80\n";
+        //when
+        Student newStudent = new Student(firstName,lastName,new Double[0]);
+        newStudent.addExamScore(97.80);
+        String actualExamScoresString = newStudent.getExamScores();
+
+        //then
+        Assert.assertEquals(expectedExamScores,actualExamScoresString);
+    }
+
+    @Test
+    public void addExamScoreToFilledLitCheckString(){
+        //given
+        String firstName = "Marci";
+        String lastName = "B";
+        String expectedExamScores = "Exam Scores:\n" +
+                "       Exam 1 -> 97.80\n" +
+                "       Exam 2 -> 92.00\n" +
+                "       Exam 3 -> 76.90\n" +
+                "       Exam 4 -> 100.00\n";;
+        //when
+        Student newStudent = new Student(firstName,lastName,new Double[]{97.8, 92.0, 76.9});
+        newStudent.addExamScore(100.0);
+        String actualExamScoresString = newStudent.getExamScores();
+
+        //then
+        Assert.assertEquals(expectedExamScores,actualExamScoresString);
+    }
+
+    @Test
+    public void addExamScoreTestCheckIndex(){
+        //given
+        String firstName = "Marci";
+        String lastName = "B";
+        Double expectedExamScores = 100.0;
+        //when
+        Student newStudent = new Student(firstName,lastName,new Double[]{97.8, 92.0, 76.9});
+        newStudent.addExamScore(expectedExamScores);
+        Double actualExamScores = newStudent.getExamScore(3);
+
+        //then
+        Assert.assertEquals(expectedExamScores,actualExamScores);
+    }
+
+    @Test
+    public void addExamScoreTestWithNull(){
+        //given
+        String firstName = "Marci";
+        String lastName = "B";
+        List<Double> expectedExamList = new ArrayList<>();
+        expectedExamList.add(null);
+        expectedExamList.add(92.0);
+        expectedExamList.add(null);
+        String expectedExamScores = "Exam Scores:\n" +
+                "       Exam 1 -> null\n" +
+                "       Exam 2 -> 92.00\n" +
+                "       Exam 3 -> null\n"+
+                "       Exam 4 -> null\n";
+        //when
+        Student newStudent = new Student(firstName,lastName,new Double[]{null, 92.0,null});
+        newStudent.addExamScore(null);
+        String actualExamScoresString = newStudent.getExamScores();
+
+        //then
+        Assert.assertEquals(expectedExamScores,actualExamScoresString);
+    }
+
+    @Test
+    public void setExamScoreTest(){
+        //given
+        String firstName = "Marci";
+        String lastName = "B";
+        String expectedExamScores = "Exam Scores:\n" +
+                "       Exam 1 -> 99.00\n" +
+                "       Exam 2 -> 92.00\n" +
+                "       Exam 3 -> null\n";
+        //when
+        Student newStudent = new Student(firstName,lastName,new Double[]{null, 92.0,null});
+        newStudent.setExamScore(0,99.0);
+        String actualExamScoresString = newStudent.getExamScores();
+
+        //then
+        Assert.assertEquals(expectedExamScores,actualExamScoresString);
+    }
+
+    @Test
+    public void getAverageExamScore(){
+        //given
+        Double expectedAverage = 89.0;
+        Student newStudent = new Student("","",new Double[]{97.8, 92.0, 76.9});
+        //when
+        Double actual = newStudent.getAverageExamScore();
+        //then
+        Assert.assertEquals(expectedAverage,actual);
+    }
+
+    @Test
+    public void getAverageExamScoreWhen0(){
+        //given
+        Student newStudent = new Student();
+        Double expectedAverage = 0.0;
+        //when
+        Double actual = newStudent.getAverageExamScore();
+        //then
+        Assert.assertEquals(expectedAverage, actual);
+
+    }
+
+    @Test
+    public void toStringEmptyStudentTest(){
+        //given
+        Student newStudent = new Student();
+        String expectedToString = "Student Name: Marshilla Brahma\n>Average Score: 0.0\n>Exam Scores:\n";
+        //when
+        String actualToString = newStudent.toString();
+        //then
+        Assert.assertEquals(expectedToString,actualToString);
+    }
+
+    @Test
+    public void toStringStudentWithNoExamsTest(){
+        //given
+        Student newStudent = new Student("Marci", "Brahma",new Double[]{100.0, 74.5});
+        String expectedToString = "Student Name: Marci Brahma\n>Average Score: 0\n>Exam Scores:\n"+
+                "Exam 1 -> 100.00\n" +
+                "Exam 2 -> 74.50\n";
+        //when
+        String actualToString = newStudent.toString();
+        //then
+        Assert.assertEquals(expectedToString,actualToString);
+    }
+
+    @Test
+    public void toStringStudentWithExamsTest(){
+        //given
+        Student newStudent = new Student();
+        //when
+
+        //then
+    }
+
 
 }
